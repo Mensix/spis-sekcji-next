f1d369026d8e1ea210d451c1521be2eda40ce2fa
Mensix <grzegorz.perun@gmail.com>
2020-08-22 16:58:53 +0200

    feat: add taggroups view

---
+++ pages/taggroups.vue
@@ -0,0 +1,155 @@
+<template>
+  <q-table
+    binary-state-sort
+    :columns="table.columns"
+    :data="dataset.groups"
+    dense
+    :filter="table.search"
+    flat
+    :loading="table.isLoading"
+    :pagination.sync="table.pagination"
+    :rows-per-page-options="[]"
+    :visible-columns="['name', 'members', 'membersGrowth', 'link']"
+  >
+    <template v-slot:top-left>
+      <div
+        :class="{
+          'q-py-sm': $q.screen.lt.xs || $q.screen.lt.sm,
+          'q-py-lg': $q.screen.lt.md || $q.screen.lt.lg || $q.screen.lt.xl,
+        }"
+      >
+        <q-input
+          v-model="table.search"
+          class="q-mb-sm"
+          color="secondary"
+          :debounce="250"
+          dense
+          label="Wyszukiwarka grup"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        <p class="q-ma-none">Autorzy: Grzegorz Perun & Daniel Nguyen</p>
+        <p
+          :class="{
+            'q-ma-none': true,
+            'text-white': dataset.lastUpdateDate.length == 0,
+          }"
+        >
+          Ostatnia aktualizacja: {{ dataset.lastUpdateDate }}
+        </p>
+      </div>
+    </template>
+
+    <template v-slot:header="props">
+      <q-tr :props="props">
+        <q-th class="no-border" />
+        <q-th key="members" class="no-border" :props="props">
+          {{ props.cols[1].label }}
+        </q-th>
+        <q-th class="no-border" />
+        <q-th class="no-border" />
+      </q-tr>
+      <q-tr :props="props">
+        <q-th key="name" :props="props">{{ props.cols[0].label }}</q-th>
+        <q-th key="membersGrowth" :props="props">
+          {{ props.cols[3].label }}
+        </q-th>
+        <q-th key="link" :props="props">{{ props.cols[2].label }}</q-th>
+        <q-th key="category" :props="props"> {{ props.cols[3].label }}</q-th>
+      </q-tr>
+    </template>
+
+    <template v-slot:body-cell-name="props">
+      <q-td :props="props">
+        <small class="text-grey q-mr-xxs">{{ props.row.index }}. </small>
+        <q-icon
+          v-if="props.row.members >= 10000"
+          class="q-mr-xxs"
+          color="secondary"
+          name="star"
+        />
+        <span>{{ props.row.name }}</span>
+      </q-td>
+    </template>
+
+    <template v-slot:body-cell-members="props">
+      <q-td :props="props">
+        <span class="q-mr-xxs">{{ props.row.members }}</span>
+        <small
+          v-if="props.row.membersGrowth"
+          :class="{
+            'text-green': props.row.membersGrowth > 0,
+            'text-red': props.row.membersGrowth < 0,
+          }"
+        >
+          <q-icon
+            :name="
+              props.row.membersGrowth > 0
+                ? 'arrow_upward'
+                : props.row.membersGrowth < 0
+                ? 'arrow_downward'
+                : null
+            "
+          />
+          <span>
+            {{
+              props.row.membersGrowth > 0
+                ? `+${props.row.membersGrowth}`
+                : props.row.membersGrowth &lt; 0
+                ? props.row.membersGrowth
+                : null
+            }}
+          </span>
+        </small>
+      </q-td>
+    </template>
+
+    <template v-slot:body-cell-membersGrowth />
+
+    <template v-slot:body-cell-link="props">
+      <q-td :props="props">
+        <a
+          class="text-secondary"
+          :href="`https://facebook.com/groups/${props.row.link}`"
+          rel="noopener noreferer"
+          target="_blank"
+        >
+          /{{ props.row.link }}
+        </a>
+      </q-td>
+    </template>
+  </q-table>
+</template>
+
+<script>
+import { onMounted } from '@nuxtjs/composition-api'
+import useGroups from '~/shared/useGroups'
+import useTable from '~/shared/useTable'
+export default {
+  layout: 'main',
+  setup() {
+    const { table } = useTable()
+    const { dataset } = useGroups()
+
+    onMounted(() =>
+      fetch('https://spissekcji.firebaseio.com/taggroups.json')
+        .then((response) => response.json())
+        .then((output) => {
+          dataset.lastUpdateDate = output.lastUpdateDate
+          dataset.groups = output.groups.map((_, idx) => ({
+            ..._,
+            index: idx + 1,
+          }))
+        })
+        .then(() => (table.isLoading = false))
+    )
+
+    return {
+      table,
+      dataset,
+    }
+  },
+}
+</script>
