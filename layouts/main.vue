85f6680566f0a90e3e13a902657624b611a7932e
Mensix <grzegorz.perun@gmail.com>
2020-08-31 09:35:38 +0200

    release: v2.0.0-beta.1

--- layouts/main.vue
+++ layouts/main.vue
@@ -91,7 +91,7 @@
       </p>
       <q-separator class="q-my-md" />
       <p class="q-ma-none">
-        <span>v2.0.0-alpha.11</span>
+        <span>v2.0.0-beta.1</span>
         <span> | </span>
         <a
           class="text-secondary"
