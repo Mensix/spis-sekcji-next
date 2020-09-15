f8e52f8ad1df1d89eb5153272db35f46deb175b4
Mensix <grzegorz.perun@gmail.com>
2020-09-15 18:06:04 +0200

    chore: uncomment linters settings files

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
   rules: {},
 }
