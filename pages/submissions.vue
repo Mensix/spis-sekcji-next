07e3b5645a7a1410b13b6d0f611e0ac10faac770
Mensix <grzegorz.perun@gmail.com>
2020-09-17 15:07:25 +0200

    feat(submissions): update sent date pushing

--- pages/submissions.vue
+++ pages/submissions.vue
@@ -271,7 +271,7 @@ export default {
         .ref('submissions')
         .child(form.type === 'Sekcja' ? 'sections' : 'taggroups')
         .push({
-          date: format(new Date(), 'dd/MM/yyyy H:m'),
+          date: format(new Date(), 'dd/MM/yyyy kk:mm'),
           update: true,
           category:
             form.type === 'Sekcja' && form.category.length === 1
