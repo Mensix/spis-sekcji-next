4933a6a6b21c12480277064283e7c9f69b0a8482
Grzegorz02@ <grzegorz.perun@gmail.com>
2021-05-11 13:42:17 +0200

    fix(privacy): fix prettier issue

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -7,7 +7,11 @@
     }"
   >
     <div
-      class="text-grey q-gutter-y-lg col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"
+      class="
+        text-grey
+        q-gutter-y-lg
+        col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4
+      "
     >
       <div>
         <h5
