b89a1bdcd4a05c3c4feb0d33449b8d8797db7000
Grzegorz02@ <grzegorz.perun@gmail.com>
2021-05-31 10:16:47 +0200

    fix(archive): fix counting weeks
    Just an another attempt.

--- components/archive.vue
+++ components/archive.vue
@@ -107,7 +107,7 @@ export default {
           )
           twoWeeksPeriods =
             (twoWeeksPeriods - (twoWeeksPeriods % 2)) / 2 +
-            ([0, 1, 2, 3, 4, 5].includes(getDay(new Date())) ? 1 : 0)
+            ([2, 3, 4, 5].includes(getDay(new Date())) ? 1 : 0)
 
           groupData.value = { ...snapshot.val(), dates: [] }
           const missingHistoryItems =
