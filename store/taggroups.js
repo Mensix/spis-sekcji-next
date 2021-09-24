d2066e07371e7b9ec8803ea3e82ee9b959d4f09d
Grzegorz02@ <grzegorz.perun@gmail.com>
2021-09-24 21:38:00 +0200

    fix(taggroups): map members property by default

--- store/taggroups.js
+++ store/taggroups.js
@@ -16,7 +16,8 @@ const fetchGroups = () => {
       dataset.lastUpdateDate = snapshot.val().lastUpdateDate
       dataset.groups = snapshot
         .val()
-        .groups.sort((e, a) => a.members - e.members)
+        .groups.map((x) => ({ ...x, members: x.members || 0 }))
+        .sort((e, a) => a.members - e.members)
         .map((_, idx) => ({
           ..._,
           index: idx + 1,
